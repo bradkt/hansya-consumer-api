8f3c67f5a562425cd7f5e365f3d01b6d4e8f582b
Matt Laptop <westlake_m@yahoo.com>
2017-01-28 18:48:53 -0500

    trying CI with a local instance

--- config/env/ci.js
+++ config/env/ci.js
@@ -18,7 +18,7 @@ module.exports = {
    ***************************************************************************/
 
   models: {
-    connection: 'ciDatabase',
+    connection: 'testLocal',
     migrate: 'drop'
   }
 };
